f79d4da5e389e028834b9fc33f89b6cc7f4f8fb1
Rebekkahleth <146350018+Rebekkahleth@users.noreply.github.com>
2023-12-12 16:19:09 +0100

    chat list styling

--- msg-app/src/pages/ChatListPage.jsx
+++ msg-app/src/pages/ChatListPage.jsx
@@ -131,19 +131,22 @@ const ChatListPage = () => {
           {filteredChats.map(chat => {
             const recentContent = recentMessages[chat.id] ? recentMessages[chat.id].get('content') : '';
             const words = recentContent.split(' ');
-            const previewText = words.length > 7 ? `${words.slice(0, 7).join(' ')} ...` : recentContent;
+            const previewText = words.length > 5 ? `${words.slice(0, 5).join(' ')} ...` : recentContent;
 
             return (
               <div key={chat.id} className="message-list">
                 <div className="profilepic">{getOtherUserFullName(chat)[0]}</div>
-                <div className='user-details'>
+                <div className='chat-flex'>
                   <Link to={`/chat_room/${chat.id}`}>
-                    {getOtherUserFullName(chat)}
-                  </Link>
+                    {getOtherUserFullName(chat)} </Link>
+
+                  <div className='user-details'>
                     <div className="previewInfo">{previewText}</div>
-              </div>
-              <div className="timestamp">{formatTimestamp(recentMessages[chat.id]?.createdAt)}</div>
+                    <div className="timestamp">{formatTimestamp(recentMessages[chat.id]?.createdAt)}</div>
+                  </div>
                 </div>
+
+              </div>
             );
           })}
         </div>
